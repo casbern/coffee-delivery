9848ad52b610d5bba47b4f34376bfebe3e7bcc0c
Cássia <cascrisbern@gmail.com>
2024-03-21 19:45:13 +0200

    added react-router-dom added Router file and the routes added pages added a default layout file

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { ThemeProvider } from "styled-components"
 import { defaultTheme } from "./styles/default"
 import { GlobalStyle } from "./styles/global"
+import { BrowserRouter } from "react-router-dom"
+import { Router } from "./Router"
 
 export function App() {
 
   return (
     <ThemeProvider theme={defaultTheme}>
-      <p>Hello World</p>
+      <BrowserRouter>
+      <Router />
       <GlobalStyle />
+      </BrowserRouter>
     </ThemeProvider>
   )
 }
