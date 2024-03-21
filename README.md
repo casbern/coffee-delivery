46891a6be62836db9e77f472bc88ecc3f71daceb
Cássia <cascrisbern@gmail.com>
2024-03-21 11:25:50 +0200

    added favicon deleted content of readme added styled-component added global styles and theme provider

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # Coffee Delivery
 
-# coffee-delivery
---
+++ src/styles/global.ts
@@ -0,0 +1,31 @@
+import { createGlobalStyle } from "styled-components"
+
+export const GlobalStyle = createGlobalStyle`
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+html {
+  font-size: 62.5%;
+}
+
+:focus {
+  outline: 0;
+  box-shadow: 0 0 0 2px ${ props => props.theme['yellow-dark']};
+}
+
+body {
+  background: #333;
+  color: #fff;
+}
+
+body, input, textarea, button {
+  font-family: "Roboto", sans-serif;  
+  font-weight: 400;
+  font-size: 1.6rem;
+}
+
+`
