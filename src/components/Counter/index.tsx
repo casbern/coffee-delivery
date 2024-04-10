2caa6df9f1eb697214ebf1f66603a284f34925a3
Cássia <cascrisbern@gmail.com>
2024-04-10 17:13:09 +0300

    added logic to add or subtract the quantity in the counter

--- src/components/Counter/index.tsx
+++ src/components/Counter/index.tsx
@@ -1,12 +1,25 @@
 import { Minus, Plus } from 'phosphor-react'
 import { CounterContainer } from './styles'
+import { useState } from 'react'
 
 export function Counter() {
+  const [counter, setCounter] = useState(0)
+
+  function handleMinusClick() {
+    if (counter > 0) {
+      setCounter(counter - 1)
+    }
+  }
+
+  function handlePlusClick() {
+    setCounter(counter + 1)
+  }
+
   return (
     <CounterContainer>
-      <Minus size={14} />
-      <span>0</span>
-      <Plus size={14} />
+      <Minus size={14} onClick={handleMinusClick} />
+      <span>{counter}</span>
+      <Plus size={14} onClick={handlePlusClick} />
     </CounterContainer>
   )
 }
